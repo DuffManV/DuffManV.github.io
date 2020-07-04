e8d42c7772aa6ece3282422f8baa38c808a15fc4
DuffManV <52357490+DuffManV@users.noreply.github.com>
2020-07-04 08:14:38 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -24,23 +24,10 @@ $(function () {
             });
 
         }
-        if (screen.width<767){
-            let scrollPos = 0;
-            $(window).scroll(function () {
-                let scroll = $(this).scrollTop();
-                if (scroll> scrollPos){
-                    $('.nav__row-top').slideUp();
-                }
-                else {
-                    $('.nav__row-top').slideDown();
-                }
-                scrollPos = scroll;
-            });
-        }
         else {
             $('.places__slider').slick({
                 slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToScroll: 1,
                 autoplay: false,
                 dots: true,
             });
@@ -53,7 +40,23 @@ $(function () {
                 dots: true,
 
             });
-    }
+        }
+        if (screen.width<768){
+            let scrollPos = 0;
+            $(window).scroll(function () {
+                if ($('.nav__navigation').hasClass('nav__navigation-show'))
+                    return false;
+                let scroll = $(this).scrollTop();
+                if (scroll> scrollPos){
+                    $('.nav__row-top').slideUp();
+                }
+                else {
+                    $('.nav__row-top').slideDown();
+                }
+                scrollPos = scroll;
+            });
+        }
+
 
 
     $('.popular__slider').slick({
@@ -83,16 +86,17 @@ $(function () {
     }
 
     $('.header__burger').click(function () {
-        $('.nav__row-top').hide();
+        $('.nav__row-top').toggle();
         $('.nav__navigation')
             .toggleClass('nav__navigation-show');
+
         $('html')
-            .toggleClass('lock')
+            .toggleClass('fixed')
     });
 
     jQuery.fn.modalWindow = function () {
         this.click(function () {
-            let tripForm = document.querySelector('.trip')
+            let tripForm = document.querySelector('.trip');
             let name = $(this).attr('data-name');
             if ($(this).attr('data-trip')){
                 document.querySelector('.trip-name').value = $(this).attr('data-trip');
@@ -100,11 +104,13 @@ $(function () {
             }
 
             $('body').addClass('modal');
+            $('html').addClass('fixed');
             $('.modal__window-'+name).addClass('show');
         });
         $('.modal__window>span').click(function () {
             $('.modal__window').removeClass('show');
             $('body').removeClass('modal');
+            $('html').removeClass('fixed');
             document.querySelector('.trip').setAttribute('hidden', 'hidden');
         });
     };
